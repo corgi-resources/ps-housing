b6286abf36de5ae94bf7302e11227ed9d42d75ec
Guedes <80121559+thejoaoguedes@users.noreply.github.com>
2023-08-23 14:42:12 -0300

    Update config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -9,8 +9,8 @@ Config = {}
 -- If you're not utilizing ox_lib, it's time to question your approach.
 Config.Target = "ox" -- "ox" or "qb"
 Config.Notify = "ox" -- "ox" or "qb"
-Config.Radial = "ox" -- "ox" or "qb"
-Config.Inventory = "qb" -- "ox" or "qb"
+Config.Radial = "qb" -- "ox" or "qb"
+Config.Inventory = "ox" -- "ox" or "qb"
 Config.Logs = "qb" -- "qb"
 
 -- Anyone provided with keys to a property has the ability to modify its furnishings.
@@ -67,7 +67,7 @@ Config.Apartments = {
         },
         shell = "Apartment Furnished",
     },
-    
+
     ["South Rockford Drive"] = {
         label = "South Rockford Drive",
         door = { x = -667.02, y = -1105.24, z = 14.63, h = 242.32, length = 1, width = 2 },
@@ -124,7 +124,7 @@ Config.Shells = {
         hash = `standardmotel_shell`,
         doorOffset = { x = -0.5, y = -2.3, z = 0.0, h = 90.0, width = 1.5 },
         stash = {
-            maxweight = 100000, 
+            maxweight = 100000,
             slots = 12,
         },
         imgs = {
@@ -140,7 +140,7 @@ Config.Shells = {
         hash = `modernhotel_shell`,
         doorOffset = { x = 4.98, y = 4.35, z = 0.0, h = 179.79, width = 2.0  },
         stash = {
-            maxweight = 150000, 
+            maxweight = 150000,
             slots = 20,
         },
         imgs = {
@@ -160,7 +160,7 @@ Config.Shells = {
         hash = `furnitured_midapart`,
         doorOffset = { x = 1.44, y = -10.25, z = 0.0, h = 0.0, width = 1.5  },
         stash = {
-            maxweight = 200000, 
+            maxweight = 200000,
             slots = 25,
         },
         imgs = {
@@ -196,7 +196,7 @@ Config.Shells = {
         hash = `shell_v16mid`,
         doorOffset = { x = 1.34, y = -14.36, z = -0.5, h = 354.08, width = 1.5  },
         stash = {
-            maxweight = 200000, 
+            maxweight = 200000,
             slots = 25,
         },
         imgs = {
@@ -228,7 +228,7 @@ Config.Shells = {
         hash = `shell_v16low`,
         doorOffset = { x = 4.69, y = -6.5, z = -1.0, h = 358.50, width = 1.5  },
         stash = {
-            maxweight = 200000, 
+            maxweight = 200000,
             slots = 25,
         },
         imgs = {
@@ -252,7 +252,7 @@ Config.Shells = {
         hash = `shell_garagem`,
         doorOffset = { x = 14.0, y = 1.7, z = -0.76, h = 88.49, width = 2.0  },
         stash = {
-            maxweight = 500000, 
+            maxweight = 500000,
             slots = 50,
         },
         imgs = {
@@ -272,7 +272,7 @@ Config.Shells = {
         hash = `shell_office1`,
         doorOffset = { x = 1.2, y = 4.90, z = -0.73, h = 180.0, width = 2.0  },
         stash = {
-            maxweight = 250000, 
+            maxweight = 250000,
             slots = 30,
         },
         imgs = {
@@ -292,7 +292,7 @@ Config.Shells = {
         hash = `shell_store1`,
         doorOffset = { x = -2.69, y = -4.56, z = -0.62, h = 1.91, width = 2.0  },
         stash = {
-            maxweight = 500000, 
+            maxweight = 500000,
             slots = 50,
         },
         imgs = {
@@ -312,7 +312,7 @@ Config.Shells = {
         hash = `shell_warehouse1`,
         doorOffset = { x = -8.96, y = 0.11, z = -0.95, h = 270.64, width = 2.0  },
         stash = {
-            maxweight = 1000000, 
+            maxweight = 1000000,
             slots = 100,
         },
         imgs = {
@@ -332,7 +332,7 @@ Config.Shells = {
         hash = `container_shell`,
         doorOffset = { x = 0.05, y = -5.7, z = -0.22, h = 1.7, width = 2.2  },
         stash = {
-            maxweight = 500000, 
+            maxweight = 500000,
             slots = 100,
         },
         imgs = {
@@ -348,7 +348,7 @@ Config.Shells = {
         hash = `shell_michael`,
         doorOffset = { x = -9.6, y = 5.63, z = -4.07, h = 268.55, width = 2.0  },
         stash = {
-            maxweight = 300000, 
+            maxweight = 300000,
             slots = 50,
         },
         imgs = {
@@ -396,7 +396,7 @@ Config.Shells = {
         hash = `shell_frankaunt`,
         doorOffset = { x = -0.34, y = -5.97, z = -0.57, h = 357.23, width = 2.0  },
         stash = {
-            maxweight = 200000, 
+            maxweight = 200000,
             slots = 50,
         },
         imgs = {
@@ -436,7 +436,7 @@ Config.Shells = {
         hash = `shell_ranch`,
         doorOffset = { x = -1.23, y = -5.54, z = -1.1, h = 272.21, width = 2.0  },
         stash = {
-            maxweight = 500000, 
+            maxweight = 500000,
             slots = 50,
         },
         imgs = {
@@ -468,7 +468,7 @@ Config.Shells = {
         hash = `shell_lester`,
         doorOffset = { x = -1.61, y = -6.02, z = -0.37, h = 357.7, width = 2.0  },
         stash = {
-            maxweight = 15000, 
+            maxweight = 15000,
             slots = 20,
         },
         imgs = {
@@ -492,7 +492,7 @@ Config.Shells = {
         hash = `shell_trevor`,
         doorOffset = { x = 0.2, y = -3.82, z = -0.41, h = 358.4, width = 2.0  },
         stash = {
-            maxweight = 10000, 
+            maxweight = 10000,
             slots = 12,
         },
         imgs = {
@@ -528,7 +528,7 @@ Config.Shells = {
         hash = `shell_trailer`,
         doorOffset = { x = -1.27, y = -2.08, z = -0.48, h = 358.84, width = 2.0  },
         stash = {
-            maxweight = 10000, 
+            maxweight = 10000,
             slots = 12,
         },
         imgs = {
